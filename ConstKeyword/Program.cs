5dcfe2f77b13992c44cfa4b2e899e01bad02eb4c
OZAN ERHAN YAPRAK <OzanErhanYaprak@hotmail.com>
2024-06-09 23:59:06 +0300

    #

---
+++ ConstKeyword/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstKeyword
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//const keyword bir degerin hem compile zamaninda hemde runtime zamaninda degerin degistirilememesini saglar.
+			//CLASS,STRUCT,ARRAY,METHOD VE PROPERTYLERE CONST KEYWORD UYGULAMASI YAPILAMAZ !!
+
+		}
+
+		internal class kare
+		{
+			public const double PiDegeri = 3.14; //Mutlaka bir deger atamasi yapmamiz gerekmekte.
+			public const string Isim = "Ozan";
+			public const int Yas = 33;
+			public const bool Evli = false;
+
+			//Yukarida yazilmis olan degiskenlerin hic bir tanesi const keywordunden dolayi baska bir yerde degistirilemez.
+		}
+	}
+}
---
+++ IsVeAsOperatorleri/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsVeAsOperatorleri
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Object icerisine her turlu veri tipini alir.
+			Object[] dizin = { "ozan", 5, 6.7, false, "yaprak", true, 40 };
+
+			#region IS Operatoru
+			foreach (var item in dizin)
+			{
+				if (item is string) { Console.WriteLine(item); }
+				//Dizin icerisinde string veri tipleri var ise bunlari konsola yazdirir.
+				//Burada veri tipini ne belirtirsek onlari dizin icerisinde arayip konsola yazdiracak.
+				if (item is int) { Console.WriteLine(item); }
+
+				//IsNullOrWhiteSpace() veya IsNullOrEmpty() yerine asagidaki gibide bir kullanim yapilabilinir.
+				if (item is null) { Console.WriteLine("null deger bulunuyor."); }
+			}
+			Console.ReadKey();
+			#endregion
+			#region AS Operatoru
+			foreach (var item in dizin)
+			{
+				string veri = item as string; // Bir tipi bir tipe donusturur, degil ise null atayacak.
+				Console.WriteLine(veri);
+				Console.ReadKey();
+			}
+			#endregion
+		}
+	}
+}
