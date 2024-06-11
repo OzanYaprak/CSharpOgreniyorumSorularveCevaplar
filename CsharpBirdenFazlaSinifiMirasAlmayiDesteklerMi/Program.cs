3c72a81e0e4cf619f353c9078ea2d115d14ef433
OZAN ERHAN YAPRAK <OzanErhanYaprak@hotmail.com>
2024-06-11 22:26:36 +0300

    #

---
+++ CsharpBirdenFazlaSinifiMirasAlmayiDesteklerMi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBirdenFazlaSinifiMirasAlmayiDesteklerMi
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//C# da bir sinif birden fazla sinifi mmiras alamaz sadece tek bir sinifi miras alabilir.
+			//Fakat birden fazla interface i miras alabilir !
+		}
+
+		private class childClassOne: MainOne, MainTwo
+		{
+
+		}
+		private class childClassTwo : BaseClassOne/*BaseClassTwo*/ //Bu BaseClassTwo miras alinamaz !
+		{
+
+		}
+		private class childClassThree : BaseClassOne,MainOne,MainTwo //Birden fazla interface alabilir
+		{
+
+		}
+
+
+		interface MainOne { }
+		interface MainTwo { }
+
+		class BaseClassOne { }
+		class BaseClassTwo { }
+	}
+}
---
+++ NestedTypeNedir/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedTypeNedir
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Nested type ic ice tipler olarak cevrilebilinir, ornegin bir class in icerisinde bir class tanimlarsak, bu class in icersinde tanimladigimiz class nested type bir class oluyor.
+
+			//Nested classtan bir nesne ornegi alma islemi asagidaki gibidir ;
+			uye.editor editor = new uye.editor();
+		}
+
+		internal class uye
+		{
+            public string name { get; set; }
+
+            public class editor // Bu nested class 
+			{
+                public string editorName { get; set; }
+            }
+		}
+	}
+}
