20536ba3058a793a5580d76e226d90be2ab979c5
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2024-06-27 15:14:06 +0300

    #

---
+++ SealedClassNedir/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClassNedir
+{
+	internal class Program
+	{
+		//Sealed classlar miras alınamayan classlar anlamına gelir.
+
+		static void Main(string[] args)
+		{
+		}
+	}
+
+
+	public sealed class baseClass
+	{
+		public string Name()
+		{
+			return "ozan";
+		}
+	}
+
+
+	public class chidlClass : baseClass // baseClass sealed keyword sahibi olduğu için miras alınamaz
+	{
+
+	}
+
+}
---
+++ SealedMethodNedir/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedMethodNedir
+{
+	internal class Program
+	{
+		//sealed methodlar override edilemeyen methodlar anlamına gelir
+
+		static void Main(string[] args)
+		{
+		}
+	}
+
+
+	public class baseClass
+	{
+		public string Name { get; set; }
+
+		public virtual string getName()
+		{
+			return Name;
+		}
+	}
+
+	public class firstChildClass : baseClass
+	{
+		public sealed override string getName() //method burada sealed olarak tanımlandığı için içinde bulunduğu class'ı miras alacak diğer classlar bu methodu ezemeyecek yani override edemeyecekler.
+		{
+			//return base.getName(); //base keyword miras alınan class'ı temsil eder, burada methodun içini miras aldığı sınıf içerisindeki aynı isimli methodun gövdesiyle dolduruyor.
+
+			return Name.ToUpper();
+		}
+	}
+
+	public class secondChildClass : firstChildClass
+	{
+	}
+}
