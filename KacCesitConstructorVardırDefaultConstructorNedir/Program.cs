cb2cbda61760aa4f6b5618f1bcb0c738476eb3f5
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2024-06-28 16:39:13 +0300

    #

---
+++ KacCesitConstructorVardırDefaultConstructorNedir/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KacCesitConstructorVardırDefaultConstructorNedir
+{
+	internal class Program
+	{
+		//Toplam 5 adet constructor bulunuyor ;
+		// - Default Constructor
+		// - Parameterized Constructor
+		// - Copy Constructor
+		// - Static Constructor
+		// - Private Constructor
+
+		static void Main(string[] args)
+		{
+		}
+	}
+}
