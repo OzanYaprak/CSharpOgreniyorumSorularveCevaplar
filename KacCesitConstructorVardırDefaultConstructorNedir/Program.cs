15fdcd8c8a6aa6cbc4c7cf8dae3562a45e251110
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2024-07-05 15:04:32 +0300

    #

--- KacCesitConstructorVardırDefaultConstructorNedir/Program.cs
+++ KacCesitConstructorVardırDefaultConstructorNedir/Program.cs
@@ -17,6 +17,27 @@ namespace KacCesitConstructorVardırDefaultConstructorNedir
 
 		static void Main(string[] args)
 		{
+			ornekClass ornekClass = new ornekClass();
+
+            Console.WriteLine(ornekClass.deger);
+            Console.WriteLine(ornekClass.deger2);
+        }
+	}
+
+	public class ornekClass
+	{
+		//Bir class oluşturduğumuzda arka planda otomatik olarak Default bir constructor oluşturuluyor. (System Defined Constructor)
+		//Bu constructor bu class altında oluşturduğumuz değişkenlere default değerler atanmasını sağlıyor.
+
+		public int deger;
+		public bool deger2;
+
+		//Constructorlar oluşturulurken her zaman oluşturulmuş olduğu class'ın adını alırlar !
+		public ornekClass() 
+		{
+			deger = 1;
+			deger2 = false;
 		}
+
 	}
 }
