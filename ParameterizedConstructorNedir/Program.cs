8c31eb11cf9f830ffc361e8af8501942747e9883
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2024-07-05 16:03:41 +0300

    #

---
+++ ParameterizedConstructorNedir/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterizedConstructorNedir
+{
+	internal class Program
+	{
+		//Herhangi bir Constructor'a parametre veriliyorsa bu Parameterized Constructor Oluyor.
+
+		static void Main(string[] args)
+		{
+			ornekClass ornekClass = new ornekClass("ozan",33); //Parameterized Constructor tarafında tanımlanan parametreleri burada tanımladım.
+
+			Console.WriteLine(ornekClass.Isim);
+            Console.WriteLine(ornekClass.Yas);
+        }
+	}
+
+	public class ornekClass
+	{
+        public string Isim { get; set; }
+        public int Yas { get; set; }
+
+		public ornekClass() //Aşağıda parametreli bir constructor tanımladığımız için default constructor kullanımdan kalkmış oldu, bu durumda default contructor'ı bu şekilde yeniden boş şekilde tanımlayabiliriz.
+		{
+
+		}
+
+        public ornekClass(string isim, int yas) //Parameterized Constructor
+		{
+			this.Isim = isim; //Burada this kullanmak yukarıdaki class içerisindeki Isim propertysine atama yapılacağını belirtir.
+			//veya aşağıdaki gibi de kullanılabilinir.
+			Yas = yas;
+		}
+	}
+}
