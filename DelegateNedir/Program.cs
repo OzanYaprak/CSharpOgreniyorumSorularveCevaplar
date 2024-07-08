60c88cfdf222cd6f952bca2f22bbc866950ff938
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2024-07-08 16:52:19 +0300

    #

---
+++ DelegateNedir/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateNedir
+{
+	internal class Program
+	{
+		//Delegateler bir method'a referans veren yapılardır. / Aracılık yapan methodlarda denilebilinir.
+		//Methodları daha kolay kontrol edebilmemizi ve dinamik olarak kontrol edebilmemize imkan sağlar.
+		//Bir class içerisindeki methodu delegate vasıtası ile çağırabiliriz.
+
+		static void Main(string[] args)
+		{
+		}
+	}
+}
