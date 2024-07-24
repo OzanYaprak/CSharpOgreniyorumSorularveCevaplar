4aaa6456b100d2d33941fa0d75d2be4d8a5421eb
OZAN ERHAN YAPRAK <OzanErhanYaprak@hotmail.com>
2024-07-24 23:08:24 +0300

    #

---
+++ ContinueIleBreakKeywordleriArasindakiFarkNedir/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinueIleBreakKeywordleriArasindakiFarkNedir
+{
+	internal class Program
+	{
+		// continue ve break keywordleri daha cok for dongulerinde kullanilir.
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 20; i++) 
+			{
+				if (i == 10) { break; } // burada donguden cikiliyor
+				if (i == 10) { continue; } // burada ise donguden cikilmiyor, sadece i 10 a esit oldugunda ekrana yazdirilmiyor ve diger islemler devam ediyor.
+
+                Console.WriteLine(i);
+            }
+		}
+	}
+}
--- ParamsKeywordnunAmaciNedirNasilKullanilir/Program.cs
+++ ParamsKeywordnunAmaciNedirNasilKullanilir/Program.cs
@@ -8,8 +8,30 @@ namespace ParamsKeywordnunAmaciNedirNasilKullanilir
 {
 	internal class Program
 	{
+		// Eger bir methodun sinirsiz sayida arguman almasi isteniyorsa params keyword kullanilabilinir.
+		// Bir metodun icerisine yazmis oldugumuz veriler parametredir fakat bir metodu cagirip icerisine vermis oldugumuz veriler ise argumandir.
+
 		static void Main(string[] args)
 		{
+			var intList = new int[] { 1, 2, 3, 4, 5 };
+
+			Console.WriteLine(TestMethod(intList)); // params keyword kullanilmadan hali
+
+			Console.WriteLine(TestMethod(1,2,4,5,67)); // params keyword oldugu icin artik buradan gonderegimiz veriler ornekteki gibi olabilir.
+
+			// Sadece int veri tipi icin degil, diger veri tipleri icinde kullanilabilinir.
+        }
+
+		public static int TestMethod(params int[] sayilar)
+		{
+			int toplam = 0;
+
+			foreach (int item in sayilar) 
+			{
+				toplam += item;
+			}
+
+			return toplam;
 		}
 	}
 }
