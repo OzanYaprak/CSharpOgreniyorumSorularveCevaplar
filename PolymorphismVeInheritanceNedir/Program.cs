2591f29a8030d1a432d6319ed1211fd8ace59d91
OZAN ERHAN YAPRAK <OzanErhanYaprak@hotmail.com>
2024-07-29 22:27:57 +0300

    #

---
+++ PolymorphismVeInheritanceNedir/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismVeInheritanceNedir
+{
+	internal class Program
+	{
+		// Inheritance : Bir ust sinifin ozelliklerinin bir alt sinifa gecmesidir.
+		// Polymorphism : Bir nesnenin farkli amaclar icinde kullanilabilecegi anlamina gelir. Statik ve dinamik olarak ikiye ayrilir.
+		// - Statik : Bir metotun overload olmasi demek statik cok bicimlilik deniliyor.
+		// - Dinamik : USt siniflarda kullanilan virtual abstract classlarin alt siniflarda ezilmesi.
+
+		static void Main(string[] args)
+		{
+		}
+	}
+
+	#region Statik Polymorphism
+
+	public class TestClass
+	{
+		public void Yazdir() 
+		{
+            Console.WriteLine("Sekil Yazdiriliyor");
+        }
+
+		public void Yazdir(string veritabani) 
+		{
+            Console.WriteLine("Sekil veritabanina yazdiriliyor. Ilgili veritabani: " + veritabani);
+        }
+	}
+
+	#endregion
+
+	#region Dinamik Polymorphism
+
+	public class BaseClass
+	{
+		public virtual void Olustur() // Virtual bir method oldugu icin alt methodlarda bunu ezebiliriz.
+		{
+			Console.WriteLine("Olusturuluyor..");
+		}
+	}
+
+	public class ChildClass : BaseClass
+	{
+		// Yukarida olusturmus oldugumuz methodu burada override keyword u yazarak ezebildik.
+		// Ayni metot a biz farkli bir is yaptirabildik, polymorphism bu oluyor. Bir nesnenin ozelliklerini farkli amaclar icin kulladirtabiliyoruz.
+		// Ana metotu cesitlendirdik, cok cesitlilik ( Polymorphism ) buradan geliyor.
+
+		public override void Olustur()
+		{
+			Console.WriteLine("Miras Olusturuluyor..");
+		}
+	}
+
+	public class ThirdChildClass : ChildClass 
+	{
+		public override void Olustur()
+		{
+            Console.WriteLine("Ozan YAPRAK");
+        }
+	}
+
+	#endregion
+}
---
+++ TemelOOPConceptleriNelerdir/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemelOOPConceptleriNelerdir
+{
+	// Encapsulation : Capsulleme
+	// Abstraction : Soyutlama
+	// Polymorphism : Cok cesitlilik
+	// Inheritance : Miras
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Kisi kisi = new Kisi();
+			kisi.Ekle("Ozan","Yaprak");
+		}
+	}
+
+	// Abstraction : Asagidaki siniflarda public olani sadece kullanici gorebiliyor, diger private olanlarda da islemler yapiliyor fakat bunlari kullanicilara gostermiyoruz. Bu kullanima Abstraction yani Soyutlama deniliyor.
+	// Encapsulation : Tek bir method icerisinde diger methodlarin cagrilmasina Encapsulation deniliyor.
+
+	public class Kisi
+	{
+		public bool Ekle(string isim, string soyisim) 
+		{
+			if (DogrulamaIslemi(isim,soyisim) == true)
+			{
+				VeritabaninaEklemeIslemi(isim, soyisim);
+				return true;
+			}
+
+			return false; 
+		}
+
+		private bool DogrulamaIslemi(string isim, string soyisim) 
+		{ 
+			// Kisi veritabaninda varmi yokmu kontrolu gerceklestirdim.
+			return true; 
+		}
+
+		private bool VeritabaninaEklemeIslemi (string isim, string soyisim)
+		{
+			// Kisi varitabanina kayit edilir.
+			return true;
+		}
+	}
+}
